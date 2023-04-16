3769e2b0624b0eceebbb5e82b3449539e965a5c8
Wiktoria Pietrzyk <pietrzykwiktoria01@gmail.com>
2023-04-16 17:58:47 +0200

    code refactoring, adding styling in the hover function

--- src/global-scripts/scripts/counter/counter.js
+++ src/global-scripts/scripts/counter/counter.js
@@ -2,12 +2,3 @@
 /// Remember to always export your functions
 /// so that they can be used in other files
 
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `script test - count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
